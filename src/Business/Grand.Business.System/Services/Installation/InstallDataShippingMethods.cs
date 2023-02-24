36a699c8c49e6e332def1a52ab056e6cb065e925
namhoaingo <namhoaingo@gmail.com>
2023-02-24 21:26:41 +0700

    Shipping Method 1. Remove Next Day Air 2. Remove 2nd Day Air

--- src/Business/Grand.Business.System/Services/Installation/InstallDataShippingMethods.cs
+++ src/Business/Grand.Business.System/Services/Installation/InstallDataShippingMethods.cs
@@ -14,19 +14,7 @@ namespace Grand.Business.System.Services.Installation
                                             Name = "Ground",
                                             Description ="Compared to other shipping methods, ground shipping is carried out closer to the earth",
                                             DisplayOrder = 1
-                                        },
-                                    new ShippingMethod
-                                        {
-                                            Name = "Next Day Air",
-                                            Description ="The one day air shipping",
-                                            DisplayOrder = 3
-                                        },
-                                    new ShippingMethod
-                                        {
-                                            Name = "2nd Day Air",
-                                            Description ="The two day air shipping",
-                                            DisplayOrder = 3
-                                        }
+                                        }                                    
                                 };
             await _shippingMethodRepository.InsertAsync(shippingMethods);
         }
